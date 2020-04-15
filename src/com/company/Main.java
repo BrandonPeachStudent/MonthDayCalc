7573a9ca82163112fbe4c457ce0f903a6f62e0db
BRandon <brandon.peach.2022@Student.lhschools.org>
2020-04-15 15:36:37 -0400

    amountOfDaysInMonth method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -24,12 +24,42 @@ public class Main {
 
         System.out.println("What month were you born? (Enter the number)");
         month = keyboard.nextInt();
-        System.out.println("What day were tou born? (Enter the number)");
+        System.out.println("What day were you born? (Enter the number)");
         dayOfMonth = keyboard.nextInt();
         System.out.println("What year were you born?");
         year = keyboard.nextInt();
-        
+
+        int monthAnswer = amountOfDaysInMonth(month, year);
+        System.out.println("There are "+monthAnswer+" days in this month");
     }
 
+    public static int amountOfDaysInMonth(int month, int year) {
+        switch(month) {
+            case 1:
+            case 3:
+            case 5:
+            case 8:
+            case 9:
+            case 10:
+            case 12:
+                return 31;
 
+            case 4:
+            case 6:
+            case 7:
+            case 11:
+                return 30;
+
+            default:
+                if ((year % 100 == 0 && year % 400 ==0) || (!(year % 4 ==0))) {
+                    return 29;
+                } else {
+                    return 28;
+                }
+
+        }
+
+    }
 }
+
+
